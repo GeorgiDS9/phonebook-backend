daa92eb868ed1935afc1b4094f785afa9dc9ce29
georgiana <gchiriac2012@gmail.com>
2025-05-31 14:46:33 -0400

    Exercise 3.20 - Add number custom validation

--- models/person.js
+++ models/person.js
@@ -22,6 +22,19 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    validate: {
+      validator: function (v) {
+        // Regex explanation:
+        // ^        - start of string
+        // \d{2,3}  - 2 or 3 digits (first part)
+        // -        - literal dash
+        // \d+      - one or more digits (second part)
+        // $        - end of string
+        return /^\d{2,3}-\d{7,8}$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Phone number must be in format XX-XXXXXXX or XXX-XXXXXXX`,
+    },
   },
 });
 
